15b86b9f570f823e33ceebbc9b4c02f3f14871d8
Diana Dima <diana.c.dima@gmail.com>
2021-05-03 19:57:56 -0400

    add plotting fct

---
+++ analysis/multiple_arrangement/functions/plot/plot_ratings.m
@@ -0,0 +1,56 @@
+function [] = plot_ratings(datapath)
+
+colours = [0.2 0.5 0.8; 0.2 0.7 0.5];
+metrics = {'Sociality', 'Transitivity', 'Valence', 'Arousal', 'Action'};
+m = cell(5,2); %read ratings in cell array
+
+for i = 1:2
+    
+    if i==1
+        load(fullfile(datapath,'videoset_152.mat'));
+    else
+        load(fullfile(datapath,'videoset_65.mat'));
+    end
+    
+    %read and reorder ratings
+    for ii = 1:4
+        
+        r = squeeze(ratingsZ(ii,:,:));
+        if ii==1
+            m{ii,i} = nanmean(r,2);
+        else
+            m{ii+1,i} = nanmean(r,2);
+        end
+    end
+    
+    m{2,i} = nanmean(transitivity,2);
+    
+end
+
+figure
+rp = rm_raincloud(m,colours,1);
+f=get(gca,'Children');
+legend([f(4),f(2)],'Exp 1','Exp 2')
+legend boxoff
+box off
+metrics = fliplr(metrics);
+yticklabels(metrics)
+set(gca,'TickLength',[0.001 0.001])
+xlabel('Z-score')
+xlim([-3 3.2])
+xticks(-3:1:3)
+set(gca,'FontSize',20)
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
--- analysis/multiple_arrangement/plot_results.m
+++ analysis/multiple_arrangement/plot_results.m
@@ -5,6 +5,7 @@ addpath(genpath(pwd))
 basepath = fileparts(fileparts(pwd));
 respath1 = fullfile(basepath, 'results','multiple_arrangement','exp1');
 respath2 = fullfile(basepath, 'results','multiple_arrangement','exp2');
+datapath = fullfile(basepath,'results','video_ratings');
 
 %% average RDMs
 
@@ -13,6 +14,10 @@ plot_rdms(respath1, respath2)
 %% data reliability
 
 plot_reliability(respath1,respath2)
+
+%% plot behavioral rating distribution
+
+plot_ratings(datapath)
 
 %% model intercorrelations
 
@@ -25,6 +30,9 @@ plot_rsa(respath1, respath2, model_idx)
 %% variance partitioning
 
 plot_varpart(respath1,respath2)
+
+
+
 
 
 
